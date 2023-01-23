f368fa56f2902398288381787a48bf735dc191b9
Karin Hogenbirk <karinhogenbirk93@gmail.com>
2023-01-23 13:31:20 +0100

    added unit test

--- dist/scraper.test.js
+++ dist/scraper.test.js
@@ -6,12 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const amsterdamRent_1 = __importDefault(require("./testdata/amsterdamRent"));
 const utils_1 = require("./utils");
 const scraper_1 = require("./scraper");
+const _1 = require(".");
 describe("Find house listings", () => {
     test("should find all ordered lists", () => {
-        //arrange
         const doc = (0, utils_1.htmlToJSDOM)(amsterdamRent_1.default);
         const listings = (0, scraper_1.parseListings)(doc);
-        //assert
         expect(listings.length).toBeGreaterThan(0);
+    });
+});
+describe("Create clean scraping result", () => {
+    test("should remove RegEx from string", () => {
+        const addressTest = "\n              Hobbemakade 30 A.\n";
+        const removedRegEx = (0, _1.removeSpaces)(addressTest);
+        expect(removedRegEx).toMatch(new RegExp("Hobbemakade 30 A."));
     });
 });
--- src/scraper.test.ts
+++ src/scraper.test.ts
@@ -1,13 +1,20 @@
 import html from "./testdata/amsterdamRent";
 import { htmlToJSDOM } from "./utils";
 import { parseListings } from "./scraper";
+import { removeSpaces } from ".";
 
 describe("Find house listings", () => {
   test("should find all ordered lists", () => {
-    //arrange
     const doc = htmlToJSDOM(html);
     const listings = parseListings(doc);
-    //assert
     expect(listings.length).toBeGreaterThan(0);
+  });
+});
+
+describe("Create clean scraping result", () => {
+  test("should remove RegEx from string", () => {
+    const addressTest = "\n              Hobbemakade 30 A.\n";
+    const removedRegEx = removeSpaces(addressTest);
+    expect(removedRegEx).toMatch(new RegExp("Hobbemakade 30 A."));
   });
 });
